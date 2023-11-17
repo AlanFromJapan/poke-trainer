4ee792056bb3ab58996c8e63e9e08c8e0d495265
MX61LW <alain.vizzini@nnlife.co.jp>
2023-11-17 13:35:04 +0900

    Added param for SSL validation skip on Speech2Text API too

--- api/api.py
+++ api/api.py
@@ -31,5 +31,5 @@ def speech2Text(lang, message):
     print(f"S2T: {lang} - '{message}'")
     req = requests.Request('GET', f"https://api.voicerss.org/?key={myconfig['VoiceRSS key']}&hl={lang}&c=MP3&v=Zola&f=16khz_16bit_mono&src={message}")
     prepared = req.prepare()
-    resp = requests.Session().send(prepared, stream=True)
+    resp = requests.Session().send(prepared, stream=True, verify=myconfig["SSL_CHECK"])
     return resp.raw.read(), resp.status_code, resp.headers.items()
